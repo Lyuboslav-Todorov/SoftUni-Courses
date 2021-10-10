c608a9e1958809e0e09010c18fcedd36702737ea
Lyuboslav-Todorov <lyuboslavtodorov@gmail.com>
2021-10-10 13:06:57 +0300

    Added java advanced code

---
+++ Java Advanced/MultidimensionalArraysExercises/StringMatrixRotation.java
@@ -0,0 +1,11 @@
+package MultidimensionalArraysExercises;
+
+import java.util.Scanner;
+
+public class StringMatrixRotation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
