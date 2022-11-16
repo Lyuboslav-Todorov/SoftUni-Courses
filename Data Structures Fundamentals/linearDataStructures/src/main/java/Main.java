421e99bdace38a3df8de1cc62c515e9b2c784093
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2022-11-16 02:55:14 +0200

    added Java code in stack class

--- Data Structures Fundamentals/linearDataStructures/src/main/java/Main.java
+++ Data Structures Fundamentals/linearDataStructures/src/main/java/Main.java
@@ -1,6 +1,17 @@
+import implementations.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
+        Stack<String> stack = new Stack<>();
+
+        stack.push("one");
+        stack.push("two");
+        stack.push("three");
+
+        for (String s : stack) {
+            System.out.println(s);
+        }
 
     }
 }
--- Data Structures Fundamentals/linearDataStructures/src/main/java/implementations/Stack.java
+++ Data Structures Fundamentals/linearDataStructures/src/main/java/implementations/Stack.java
@@ -6,33 +6,79 @@ import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
 
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> previous;
+
+        public Node(E value) {
+            this.element = value;
+        }
+    }
+
+    public Stack() {
+        this.top = null;
+        this.size = 0;
+    }
+
     @Override
     public void push(E element) {
-
+        Node<E> newNode = new Node<>(element);
+        newNode.previous = this.top;
+        this.top = newNode;
+        size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNotEmpty();
+        E element = this.top.element;
+        Node<E> temp = this.top.previous;
+        this.top.previous = null;
+        this.top = temp;
+        size--;
+        return element;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNotEmpty();
+        return this.top.element;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() < 1;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = top;
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E element = current.element;
+                this.current = this.current.previous;
+                return element;
+            }
+        };
+    }
+
+    private void ensureNotEmpty() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty cannot pop and peek!");
+        }
     }
 }
