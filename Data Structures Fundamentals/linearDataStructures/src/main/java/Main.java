b4c8e9489c10dc600299619b4837793e7ad54c74
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2022-11-18 01:12:33 +0200

    Added Java code in Queue class

--- Data Structures Fundamentals/linearDataStructures/src/main/java/Main.java
+++ Data Structures Fundamentals/linearDataStructures/src/main/java/Main.java
@@ -1,8 +1,17 @@
-
+import implementations.Queue;
 
 public class Main {
     public static void main(String[] args) {
 
+        Queue<String> queue = new Queue<>();
+
+        queue.offer("test");
+        queue.offer("name");
+        queue.offer("name2");
+
+        for (String s : queue) {
+            System.out.println(s);
+        }
 
     }
 }
--- Data Structures Fundamentals/linearDataStructures/src/main/java/implementations/Queue.java
+++ Data Structures Fundamentals/linearDataStructures/src/main/java/implementations/Queue.java
@@ -12,28 +12,51 @@ public class Queue<E> implements AbstractQueue<E> {
     private static class Node<E> {
         private E element;
         private Node<E> next;
-        private Node<E> previous;
+
+        public Node(E element) {
+            this.element = element;
+        }
     }
 
     public Queue() {
-        this.node = new Node<>();
+        this.head = null;
         this.size = 0;
     }
 
     @Override
     public void offer(E element) {
-
-        size++;
+        Node<E> newNode = new Node<>(element);
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node<E> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.size++;
     }
 
     @Override
     public E poll() {
-        return null;
+        ensureIsNotEmpty();
+        E removedElement = this.head.element;
+        if (this.size == 1){
+            this.head = null;
+        } else {
+            Node<E> newNode = this.head.next;
+            this.head.next = null;
+            this.head = newNode;
+        }
+        size--;
+        return removedElement;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureIsNotEmpty();
+        return this.head.element;
     }
 
     @Override
@@ -48,6 +71,25 @@ public class Queue<E> implements AbstractQueue<E> {
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                ensureIsNotEmpty();
+                return head != null;
+            }
+
+            @Override
+            public E next() {
+                E element = head.element;
+                head = head.next;
+                return element;
+            }
+        };
+    }
+
+    private void ensureIsNotEmpty() {
+        if (isEmpty()){
+            throw new IllegalStateException("Queue is empty");
+        }
     }
 }
