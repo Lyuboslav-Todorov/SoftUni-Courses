c054adc673bc8b3b95614296061fc5848fae2660
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2022-11-28 18:27:48 +0200

    Added Java code

--- Data Structures Fundamentals/treesRepresentationAndTraversal/src/main/java/implementations/Tree.java
+++ Data Structures Fundamentals/treesRepresentationAndTraversal/src/main/java/implementations/Tree.java
@@ -2,13 +2,41 @@ package implementations;
 
 import interfaces.AbstractTree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Tree<E> implements AbstractTree<E> {
+    private E key;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+
+    public Tree(E key, Tree<E>... children) {
+        this.key = key;
+        this.children = new ArrayList<>();
+        for (Tree<E> child : children) {
+            this.children.add(child);
+            child.parent = this;
+        }
+
+    }
 
     @Override
     public List<E> orderBfs() {
-        return null;
+        List<E> result = new ArrayList<>();
+        Deque<Tree<E>> queue = new ArrayDeque<>();
+        queue.offer(this);
+        while (queue.size() > 0) {
+            Tree<E> current = queue.poll();
+            result.add(current.key);
+            for (Tree<E> child : current.children) {
+                queue.offer(child);
+            }
+        }
+
+        return result;
     }
 
     @Override
@@ -20,8 +48,8 @@ public class Tree<E> implements AbstractTree<E> {
     public void addChild(E parentKey, Tree<E> child) {
 
     }
-	
-	@Override
+
+    @Override
     public void removeNode(E nodeKey) {
 
     }
