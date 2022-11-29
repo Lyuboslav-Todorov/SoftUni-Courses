c32f2ace14e82d2e414958b038199a0c996a879c
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2022-11-29 17:33:56 +0200

    Added Java code

--- Data Structures Fundamentals/treesRepresentationAndTraversal/src/main/java/implementations/Tree.java
+++ Data Structures Fundamentals/treesRepresentationAndTraversal/src/main/java/implementations/Tree.java
@@ -26,6 +26,9 @@ public class Tree<E> implements AbstractTree<E> {
     @Override
     public List<E> orderBfs() {
         List<E> result = new ArrayList<>();
+        if (this.key == null){
+            return result;
+        }
         Deque<Tree<E>> queue = new ArrayDeque<>();
         queue.offer(this);
         while (queue.size() > 0) {
@@ -41,17 +44,65 @@ public class Tree<E> implements AbstractTree<E> {
 
     @Override
     public List<E> orderDfs() {
-        return null;
+        List<E> result = new ArrayList<>();
+        this.doDfs(this, result);
+        return result;
+    }
+
+    private void doDfs(Tree<E> tree, List<E> result) {
+        for (Tree<E> child : tree.children) {
+            child.doDfs(child, result);
+        }
+        result.add(tree.key);
     }
 
     @Override
     public void addChild(E parentKey, Tree<E> child) {
+        Tree<E> parent = findTree(parentKey);
 
+        if (parent == null) {
+            throw new IllegalArgumentException("There is no such key");
+        }
+
+        parent.children.add(child);
     }
 
     @Override
     public void removeNode(E nodeKey) {
+        Tree<E> toRemove = findTree(nodeKey);
 
+        if (toRemove == null) {
+            throw new IllegalArgumentException("There is no such key");
+        }
+
+        for (Tree<E> child : toRemove.children) {
+            child.parent = null;
+        }
+
+        toRemove.children.clear();
+
+        Tree<E> parent = toRemove.parent;
+
+        if (parent != null) {
+            parent.children.remove(toRemove);
+        }
+
+
+    }
+
+    private Tree<E> findTree(E nodeKey) {
+        Deque<Tree<E>> queue = new ArrayDeque<>();
+        queue.offer(this);
+        while (!queue.isEmpty()) {
+            Tree<E> current = queue.poll();
+            for (Tree<E> child : current.children) {
+                if (child.key.equals(nodeKey)) {
+                    return child;
+                }
+                queue.offer(child);
+            }
+        }
+        return null;
     }
 
     @Override
