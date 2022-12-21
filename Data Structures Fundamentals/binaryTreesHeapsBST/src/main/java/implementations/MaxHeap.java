8cc66d28e61944e34d1c5a8f40049075613a0c7b
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2022-12-21 16:25:56 +0200

    Added java code

--- Data Structures Fundamentals/binaryTreesHeapsBST/src/main/java/implementations/MaxHeap.java
+++ Data Structures Fundamentals/binaryTreesHeapsBST/src/main/java/implementations/MaxHeap.java
@@ -29,15 +29,15 @@ public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
     private void heapifyUp(int index) {
         int parentIndex = getParentIndex(index);
 
-        while (getParentIndex(index) >= 0 && parentIsLess(parentIndex, index)) {
+        while (index > 0 && parentIsLess(parentIndex, index)) {
             Collections.swap(this.elements, parentIndex, index);
             index = parentIndex;
+            parentIndex = getParentIndex(index);
         }
     }
 
-
-    private boolean parentIsLess(int parentIndex, int index) {
-        return this.elements.get(parentIndex).compareTo(this.elements.get(index)) < 0;
+    private boolean parentIsLess(int parentIndex, int childIndex) {
+        return this.elements.get(parentIndex).compareTo(this.elements.get(childIndex)) < 0;
     }
 
     private int getParentIndex(int index) {
