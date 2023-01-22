61a94f4a40e5b4166a4dd7ec20080a7ca8ae2bf9
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2023-01-22 23:42:32 +0200

    Added MobiLeLeLe

---
+++ Java Web/mobiLe/src/main/java/com/example/mobilelele/model/configuration/BeanConfiguration.java
@@ -0,0 +1,7 @@
+package com.example.mobilelele.model.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+}
---
+++ Java Web/mobiLe/src/main/java/com/example/mobilelele/model/entities/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.mobilelele.model.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(generator = "uuid-string")
+    @GenericGenerator(name = "uuid-string", strategy = "ogr.hibernate.id.UUIDGenerator")
+    private String id;
+
+    public BaseEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BaseEntity setId(String id) {
+        this.id = id;
+        return this;
+    }
+}
