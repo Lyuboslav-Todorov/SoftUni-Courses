d86fe97fa0dcade4ba5a181bfcabc5d950b193a5
Lyuboslav Todorov <lyuboslavtodorov@gmail.com>
2023-01-26 22:25:16 +0200

    Added Controllers

---
+++ Java Web/mobiLe/src/main/java/mobiLeLe/web/HomeController.java
@@ -0,0 +1,16 @@
+package mobiLeLe.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/index")
+public class HomeController extends BaseController {
+
+    @GetMapping
+    public ModelAndView getHome(ModelAndView modelAndView) {
+        return super.view("index");
+    }
+}
